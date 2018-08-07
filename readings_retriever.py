a57a227567942e138c2f9dc2acd1fbfe2eec87be
BradleyHe <bhe6001@gmail.com>
2018-08-07 19:21:04 -0400

    Added data for moving 0 to 172800 (2 days) and for windows 10, 20, and 50

--- readings_retriever.py
+++ readings_retriever.py
@@ -3,12 +3,12 @@ import graph
 import matplotlib.pyplot as plt
 
 start = 0
-end = 21600
-window = 10
+end = 172800
+window = 100
 interval = 900
 delay = 300
 percentage = 0.5
-stdMultiplier = -3
+stdMultiplier = 1
 
 headers = ['vmid','subscriptionid','deploymentid','vmcreated', 'vmdeleted', 'maxcpu', 'avgcpu', 'p95maxcpu', 'vmcategory', 'vmcorecount', 'vmmemory']
 data_path = 'vmtable.csv'
@@ -19,12 +19,16 @@ vmdata = vmdata.sort_values('vmcreated')
 
 # for moving avg
 # graph.moving(vmdata, start, end, window, delay, stdMultiplier)
-# df = pd.read_csv('data/moving/moving_{0}to{1}_window={2}_delay={3}_multiplier={4}.csv'.format(start, end, window, delay, stdMultiplier))
-# df.set_index("time", inplace=True)
-# df.drop(df.tail(1).index, inplace=True)
-# df.drop(['STD', 'MA', 'delays'], axis=1).plot(ylim = (321000, 325000))
-# plt.savefig('graphs/moving/moving_{0}to{1}_window={2}_delay={3}_multiplier={4}.jpg'.format(start, end, window, delay, stdMultiplier))
-# plt.show()
+df = pd.read_csv('data/moving/moving_{0}to{1}_window={2}_delay={3}_multiplier={4}.csv'.format(start, end, window, delay, stdMultiplier))
+df.set_index("time", inplace=True)
+df.drop(df.tail(1).index, inplace=True)
+
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 12
+plt.rcParams["figure.figsize"] = fig_size
+
+df.drop(['STD', 'MA', 'delays'], axis=1).plot(ylim = (320000, 332000))
+plt.savefig('graphs/moving/moving_{0}to{1}_window={2}_delay={3}_multiplier={4}.jpg'.format(start, end, window, delay, stdMultiplier), figsize = (100, 4))
 
 # for set interval
 # graph.interval(vmdata, start, end, interval, delay)
@@ -36,10 +40,10 @@ vmdata = vmdata.sort_values('vmcreated')
 # plt.show()
 
 # for global maximum
-graph.globalMax(vmdata, start, end, window, delay, percentage)
-df = pd.read_csv('data/globalMax/globalMax_{0}to{1}_window={2}_delay={3}_percentage={4}.csv'.format(start, end, window, delay, percentage))
-df.set_index('time', inplace=True)
-df.drop(df.tail(1).index, inplace=True)
-df.drop(['delays'], axis = 1).plot(ylim = (321000, 325000))
-plt.savefig('graphs/globalMax/globalMax_{0}to{1}_window={2}_delay={3}_percentage={4}.jpg'.format(start, end, window, delay, percentage))
-plt.show()
+# graph.globalMax(vmdata, start, end, window, delay, percentage)
+# df = pd.read_csv('data/globalMax/globalMax_{0}to{1}_window={2}_delay={3}_percentage={4}.csv'.format(start, end, window, delay, percentage))
+# df.set_index('time', inplace=True)
+# df.drop(df.tail(1).index, inplace=True)
+# df.drop(['delays'], axis = 1).plot(ylim = (321000, 325000))
+# plt.savefig('graphs/globalMax/globalMax_{0}to{1}_window={2}_delay={3}_percentage={4}.jpg'.format(start, end, window, delay, percentage))
+# plt.show()
