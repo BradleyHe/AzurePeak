9e5b5c40b4dea045f626b9f19897efcaa7450472
BradleyHe <bhe6001@gmail.com>
2018-08-10 23:00:19 -0400

    fixed minor error

--- graph.py
+++ graph.py
@@ -15,7 +15,7 @@ def getStats(df, statsInterval):
 	mean = []
 
 	for num in range(minTime, maxTime, statsInterval):
-		interval = df.loc[num : num + statsInterval, 'cores']
+		interval = df.loc[num : num + statsInterval - 1, 'cores']
 		percentile = interval.quantile([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
 		time.append(num)
